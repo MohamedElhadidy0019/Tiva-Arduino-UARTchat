351d9d1bee19e769197bc7302455ce0d644331de
Mohamed Khaled <Mohamed_Khaled_Kamal@outlook.com>
2020-08-05 20:31:47 +0200

    adding the main files

---
+++ PLL_SYSTICK.h
@@ -0,0 +1,63 @@
+
+#ifndef SETUP_PLL
+#define SETUP_PLL
+
+void setupPLL(void)
+{
+
+
+    // 0) Use RCC2
+      SYSCTL_RCC2_R |=  0x80000000;  // USERCC2
+      // 1) bypass PLL while initializing
+      SYSCTL_RCC2_R |=  0x00000800;  // BYPASS2, PLL bypass
+      // 2) select the crystal value and oscillator source
+      SYSCTL_RCC_R = (SYSCTL_RCC_R &~0x000007C0)   // clear XTAL field, bits 10-6
+                     + 0x00000540;   // 10101, configure for 16 MHz crystal
+      SYSCTL_RCC2_R &= ~0x00000070;  // configure for main oscillator source
+      // 3) activate PLL by clearing PWRDN
+      SYSCTL_RCC2_R &= ~0x00002000;
+      // 4) set the desired system divider
+      SYSCTL_RCC2_R |= 0x40000000;   // use 400 MHz PLL
+      SYSCTL_RCC2_R = (SYSCTL_RCC2_R&~ 0x1FC00000)  // clear system clock divider
+                      + (4<<22);      // configure for 80 MHz clock
+      // 5) wait for the PLL to lock by polling PLLLRIS
+      while((SYSCTL_RIS_R&0x00000040)==0){};  // wait for PLLRIS bit
+      // 6) enable use of PLL by clearing BYPASS
+      SYSCTL_RCC2_R &= ~0x00000800;
+
+
+
+
+}
+
+void setupSystick()
+{
+     NVIC_ST_CTRL_R=0;
+    NVIC_ST_RELOAD_R=0x0FFFFFF;
+    NVIC_ST_CURRENT_R=0;
+    NVIC_ST_CTRL_R=0x05;
+
+     /* NVIC_ST_CTRL_R = 0;              // 1) disable SysTick during setup
+      NVIC_ST_RELOAD_R = 0x00FFFFFF;   // 2) maximum reload value
+      NVIC_ST_CURRENT_R = 0;           // 3) any write to current clears it
+      NVIC_ST_CTRL_R = 0x00000005;     // 4) enable SysTick with core clock*/
+
+}
+
+void delayMS(int n)
+{
+    while(n--)
+    {
+    NVIC_ST_RELOAD_R=80000 - 1;
+    NVIC_ST_CURRENT_R=0;
+    while((NVIC_ST_CTRL_R&0x00010000)==0){}
+    }
+
+
+}
+
+
+
+
+
+#endif
---
+++ UARTconfig.h
@@ -0,0 +1,162 @@
+/*
+ * Author:Mohamed Khaled Mohamed
+ * Code for  running UART0
+ * UART0 is normally connected to the PC through the usb connection
+ *
+ * */
+
+#ifndef UART_CONFIG
+#define UART_CONFIG
+
+#include "PLL_SYSTICK.h"
+//////////////////////* Prototypes *////////////////////////
+
+//UART0
+void UART0setup(void);
+void UART0Tx(char c);
+char UART0Rx(void);
+void printWordUART0(char *ptr);
+
+//UART1
+
+void UART1setup(void);
+void UART1Tx(char c);
+char UART1Rx(void);
+void printWordUART1(char *ptr);
+
+
+
+//
+int size(char *ptr);
+
+
+
+
+///////////////////////* Implementation *//////////////////////////
+
+
+/////////////////////////////**  UART0  **///////////////////////////
+
+
+void UART0setup(void)
+{
+    //Assuming the clock is 80 mhz ,if another value the integer and fraction registers must be recalculated and changed
+    SYSCTL_RCGCUART_R|=0x01;  // Enabling clock for UART0
+    SYSCTL_RCGCGPIO_R|=0x01;  //Enabling clock for PORTA
+    UART0_CTL_R &=~(0x01);    //Disabling the UART0 to be able to setup the UART
+    UART0_IBRD_R =520;         //Integer part of baud rate is 104 (required 9600)
+    UART0_FBRD_R =54;           //Fraction part of baud rate is 11 (required 9600)
+    UART0_CC_R=0;               // Selecting the system clock as UART clock
+    UART0_LCRH_R=0x70;         // Configuring the line control regsiter as desired (see the datasheet to know the usage oe each bit)
+    UART0_CTL_R |=0x300;       //Enabling Tx and Rx for UART0
+    UART0_CTL_R |=0x01;        //Enabling UART again after finishing setup the UART regs
+    GPIO_PORTA_DEN_R|=0x03;    // Making digital enable for PA0,PA1
+    GPIO_PORTA_AFSEL_R |=0x03;  //Enabling alternate function for PA1,PA0
+    GPIO_PORTA_PCTL_R |=0x11;    //Choosing UART function for PA1,PA0
+    delayMS(1);
+
+}
+
+void UART0Tx(char c)
+{
+    while((UART0_FR_R & 0x20) != 0 );
+    UART0_DR_R=c;
+}
+
+
+char UART0Rx(void)
+{
+    while((UART0_FR_R & 0x10) !=0);
+    char temp=UART0_DR_R;
+    return (temp);
+}
+
+
+
+
+void printWordUART0(char *ptr)
+{
+    int i=0;
+    for(i=0;i<size(ptr);i++)
+    {
+        UART0Tx(ptr[i]);
+    }
+}
+
+
+/////////////////////////////**  UART1  **///////////////////////////
+
+//Using PB0,PB1   ofr UART1
+void UART1setup(void)
+{
+    //Assuming the clock is 80 mhz ,if another value the integer and fraction registers must be recalculated and changed
+    SYSCTL_RCGCUART_R|=0x02;  // Enabling clock for UART1
+    SYSCTL_RCGCGPIO_R|=0x02;  //Enabling clock for PORTB
+    UART1_CTL_R &=~(0x01);    //Disabling the UART1 to be able to setup the UART
+    UART1_IBRD_R =520;         //Integer part of baud rate is 104 (required 9600)
+    UART1_FBRD_R =54;           //Fraction part of baud rate is 11 (required 9600)
+    UART1_CC_R=0;               // Selecting the system clock as UART clock
+    UART1_LCRH_R=0x70;         // Configuring the line control regsiter as desired (see the datasheet to know the usage oe each bit)
+    UART1_CTL_R |=0x300;       //Enabling Tx and Rx for UART1
+    UART1_CTL_R |=0x01;        //Enabling UART again after finishing setup the UART regs
+    GPIO_PORTB_DEN_R|=0x03;    // Making digital enable for PB0,PB1
+    GPIO_PORTB_AFSEL_R |=0x03;  //Enabling alternate function for PB1,PB0
+    GPIO_PORTB_PCTL_R |=0x11;    //Choosing UART function for PA1,PA0
+    delayMS(1);
+
+}
+
+void UART1Tx(char c)
+{
+    while((UART1_FR_R & 0x20) != 0 );
+    UART1_DR_R=c;
+}
+
+
+char UART1Rx(void)
+{
+    while((UART1_FR_R & 0x10) !=0);
+    char temp=UART1_DR_R;
+    return (temp);
+}
+
+
+
+
+void printWordUART1(char *ptr)
+{
+    int i=0;
+    for(i=0;i<size(ptr);i++)
+    {
+        UART1Tx(ptr[i]);
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//returns the size of a character array using a pointer to the first element of the character array
+int size(char *ptr)
+{
+    //variable used to access the subsequent array elements.
+    int offset = 0;
+    //variable that counts the number of elements in your array
+    int count = 0;
+
+    //While loop that tests whether the end of the array has been reached
+    while (*(ptr + offset) != '\0')
+    {
+        //increment the count variable
+        ++count;
+        //advance to the next element of the array
+        ++offset;
+    }
+    //return the size of the array
+    return count;
+}
+
+#endif
